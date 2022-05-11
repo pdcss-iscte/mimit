381669acd83b5409b890facef9976edd03807ef1
Pedro Custódinho <pedropestaninha@gmail.com>
2022-05-11 11:00:02 +0100

    update

--- mimitapp/urls.py
+++ mimitapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('guardarregistoutilizador/', views.guardarregistoutilizador, name="guardarregistoutilizador")
+
 ]
--- mimitapp/views.py
+++ mimitapp/views.py
@@ -1,6 +1,32 @@
+
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
-    return HttpResponse("Esta é a página de entrada da Mimit")
+    return render(request, 'mimitapp/registerutilizador.html')
+
+
+def loginutilizador(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('mimitapp:perfilutilizador'))
+        else:
+            return render(request, 'mimitapp/registerutilizador.html')
+    else:
+        return render(request, 'mimitapp/registerutilizador.html')
+
+
+
+def guardarregistoutilizador(request):
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    user = User.objects.create_user(username, email, password)
+    login(request, user)
+    return HttpResponseRedirect(reverse('votacao:perfilutilizador'))
